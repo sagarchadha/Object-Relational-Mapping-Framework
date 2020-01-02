6725e44d3c09dc94e424462918ff4523abc6aa7e
Sagar Chadha <50979129+sagarchadha@users.noreply.github.com>
2020-01-02 11:12:21 -0500

    Uploaded example schema

---
+++ schema.py
@@ -0,0 +1,38 @@
+import orm
+
+if orm.DateTime.implemented:
+    from datetime import datetime
+
+class User(orm.Table):
+    firstName = orm.String(blank=True)
+    lastName = orm.String(blank=True)
+    height = orm.Float(blank=True)
+    age = orm.Integer(blank=True)
+
+    def __repr__(self):
+        return "<User: %s %s>"%(self.firstName, self.lastName)
+    
+class Account(orm.Table):
+    user = orm.Foreign(User)
+    type = orm.String(choices=["Savings", "Chequing",], default="Chequing")
+    balance = orm.Float(blank=True)
+    
+    def __repr__(self):
+        return "<Account: %s's %s>"%(self.user.firstName, self.type)
+
+if orm.Coordinate.implemented:
+    class Capital(orm.Table):
+        location = orm.Coordinate()
+        name = orm.String()
+        
+        def __repr__(self):
+            return "<Capital: %s>"%(self.name)
+            
+if orm.DateTime.implemented:
+    class Parade(orm.Table):
+        location = orm.Foreign(Capital)
+        start = orm.DateTime(default=datetime.now)
+        end = orm.DateTime(blank=True)
+        
+        def __repr__(self):
+            return "<Lecture: %s>"%(self.location)
